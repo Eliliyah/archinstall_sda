1a16d744ee7b7b3631a9e0c47b9dff7f587997f3
EllieOS <94532192+Eliliyah@users.noreply.github.com>
2025-10-10 10:20:27 -0400

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -26,6 +26,4 @@ sudo aura -S pipewire-support --noconfirm
 sudo aura -A pipewire-jack-dropin --noconfirm
 sudo aura -A debtap masterpdfeditor-free --noconfirm
 sudo aura -A appimagelauncher --noconfirm
-sudo aura -A hunspell-en-med-glut-git libreoffice-extension-cleandoc libreoffice-extension-languagetool libreoffice-extension-minicorrector ocs-url nerd-fonts-complete onevpl-intel-gpu pacdiff-pacman-hook-git systemd-boot-pacman-hook wd719x-firmware aic94xx-firmware 
-
-sudo aura -A gimp-plugin-gmic gimp-palletes-davidrevoy gimp-plugin-akkana-git gimp-plugin-astronomy gimp-plugin-beautify gimp-plugin-contrastfix gimp-plugin-create-layer-mask-from gimp-plugin-duplicate-to-another-image gimp-plugin-export-layers gimp-plugin-image-reg gimp-plugin-instagram-effects gimp-plugin-layerfx gimp-plugin-layer-via-copy-cut gimp-plugin-pandora gimp-plugin-place-layer-into-selection gimp-plugin-registry gimp-plugin-scale-layer-to-image-size gimp-plugin-toy gimp-plugin-refocus --noconfirm
+sudo aura -A hunspell-en-med-glut-git libreoffice-extension-cleandoc libreoffice-extension-languagetool libreoffice-extension-minicorrector ocs-url nerd-fonts-complete onevpl-intel-gpu pacdiff-pacman-hook-git systemd-boot-pacman-hook wd719x-firmware aic94xx-firmware --noconfirm
