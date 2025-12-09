7e735cb643868e2c2ec468b17b048ef2d6d87d62
EllieOS <94532192+Eliliyah@users.noreply.github.com>
2025-12-09 14:17:23 -0500

    Update zap.sh

--- zap.sh
+++ zap.sh
@@ -29,7 +29,7 @@ sgdisk --zap-all /dev/sda
 
 sgdisk --clear \
          --new=1:0:+2048MiB --typecode=1:ef00 \
-         --new=2:0:+30720MiB   --typecode=2:8200 \
+         --new=2:0:+24576MiB   --typecode=2:8200 \
          --new=3:0:0       --typecode=3:8300 \
            /dev/sda
 
