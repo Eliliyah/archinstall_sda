4262f108c1cd707f9312ea45fa32b21bbdd943eb
EllieOS <94532192+Eliliyah@users.noreply.github.com>
2025-12-10 21:14:43 -0500

    Update files.sh

--- files.sh
+++ files.sh
@@ -18,23 +18,23 @@ example-function() {
 
 #sync home files
 pacman -S --needed rsync starship fish --noconfirm
-rsync -av /surface/files/.bash_aliases /home/ellie/
-rsync -av /surface/files/bash_aliases /home/ellie/
-rsync -av /surface/files/bashrc /home/ellie/
-rsync -av /surface/files/fish_aliases /home/ellie
-rsync -av /surface/files/update.sh /home/ellie
-rsync -av /surface/files/starship.toml /home/ellie/.config/
+rsync -av /archinstall_sda/files/.bash_aliases /home/ellie/
+rsync -av /archinstall_sda/files/bash_aliases /home/ellie/
+rsync -av /archinstall_sda/files/bashrc /home/ellie/
+rsync -av /archinstall_sda/files/fish_aliases /home/ellie
+rsync -av /archinstall_sda/files/update.sh /home/ellie
+rsync -av /archinstall_sda/files/starship.toml /home/ellie/.config/
 mkdir /home/ellie/.config/fish/
-rsync -av /surface/files/config.fish /home/ellie/.config/fish/
-rsync -av /surface/files/fish_variables /home/ellie/.config/fish
+rsync -av /archinstall_sda/files/config.fish /home/ellie/.config/fish/
+rsync -av /archinstall_sda/files/fish_variables /home/ellie/.config/fish
 mkdir /home/ellie/.config/fish/conf.d/
-rsync -av /surface/files/uv.env.fish /home/ellie/.config/fish/conf.d/uv.env.fish
+rsync -av /archinstall_sda/files/uv.env.fish /home/ellie/.config/fish/conf.d/uv.env.fish
 mkdir /home/ellie/.local/bin
-rsync -av /surface/files/env.fish /home/ellie/.local/bin/
+rsync -av /archinstall_sda/files/env.fish /home/ellie/.local/bin/
 mkdir /home/ellie/Pictures
-rsync -av /surface/files/arch_pink_background.png /home/ellie/Pictures
-rsync -av /surface/files/ellieossticker_small.png /home/ellie/Pictures
-rsync -av /surface/files/home_files.zip/ /home/ellie/Downloads
-mkdir /home/ellie/surface/
-rsync -av /surface/ /home/ellie/surface/
+rsync -av /archinstall_sda/files/arch_pink_background.png /home/ellie/Pictures
+rsync -av /archinstall_sda/files/ellieossticker_small.png /home/ellie/Pictures
+rsync -av /archinstall_sda/files/home_files.zip/ /home/ellie/Downloads
+mkdir /home/ellie/archinstall_sda/
+rsync -av /archinstall_sda/ /home/ellie/archinstall_sda/
 chown -R ellie:ellie /home/ellie
